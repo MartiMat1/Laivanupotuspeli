04ed96140acd243f845a04c7811daabd0dee6864
MartiMat1 <martimat@uef.fi>
2025-03-28 13:13:12 +0200

    Add files via upload

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.laivanupotuspeli","l":"LaivanUpotusPeli"},{"p":"com.example.laivanupotuspeli","l":"PeliMetodit"}];updateSearchResults();
